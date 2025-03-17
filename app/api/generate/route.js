15295850c40a633ccf315022ab82fed07f777527
iMaverick007 <alloydfernandes007@gmail.com>
2025-03-17 08:54:25 +0530

    updates

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,47 +1,30 @@
 import clientPromise from "@/lib/mongodb";
-
-export async function POST(request) {
-  try {
-    const body = await request.json();
-    const client = await clientPromise;
-    const db = client.db("shortener");
-    const collection = db.collection("urls");
-
-    const doc = await collection.findOne({ shorturl: body.shorturl });
-
-    if (doc) {
-      return new Response(
-        JSON.stringify({
-          success: false,
-          error: true,
-          message: "URL already exists",
-        }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
-    }
-
-    await collection.insertOne({
-      url: body.url,
-      shorturl: body.shorturl,
-    });
-
-    return new Response(
-      JSON.stringify({
-        success: true,
-        error: false,
-        message: "URL Generated Successfully",
-      }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
-  } catch (error) {
-    console.error(error);
-    return new Response(
-      JSON.stringify({
-        success: false,
-        error: true,
-        message: "An error occurred",
-      }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
-  }
-}
+ 
+ export async function POST(request) {
+     
+   const body = await request.json();
+   const client = await clientPromise;
+   const db = client.db("shortener");
+   const collection = db.collection("urls");
+ 
+   const doc = await collection.findOne({ shorturl: body.shorturl });
+ 
+   if (doc) {
+     return Response.json({
+       success: false,
+       error: true,
+       message: "URL already exists",
+     });
+   }
+ 
+   const result = await collection.insertOne({
+     url: body.url,
+     shorturl: body.shorturl,
+   });
+ 
+   return Response.json({
+     success: true,
+     error: false,
+     message: "URL Generated SuccessFully",
+   });
+ }
